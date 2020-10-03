1466c3b604fbac692af7935ea6eaeecdfd23309c
morozowG <python.wev.edit@gmail.com>
2020-10-03 19:52:20 +0200

    add file

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ from bs4 import BeautifulSoup as bs
 
 
 url = 'https://www.avito.ru/arhangelsk/bytovaya_elektronika'
+page = req.get(url)
 
+if page.status_code != 200:
+    print("Не удалось подключится")
+
+soup = bs(page.text, "html.parser")
+
+products = soup.findAll("a", class_="snippet-link")
+print(type(products.text))
+#with open('produc.txt', "w") as w:
+#    w.write((products)
+#    w.close
 
